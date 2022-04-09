10d05ea4a87c7dc9e8230cae8d6b632e08116acf
EnanDorta <junior.goncalves01@hotmail.com>
2022-04-08 23:53:42 -0300

    Ouvindo eventos no Stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,9 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-export default (request: NextApiRequest, response: NextApiResponse) => {
+import Stripe from 'stripe'
+
+import { stripe } from '../../service/stripe'
+
+import { Readable } from 'stream'
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks)
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+    
+  }
+}
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const buf = await buffer(request)
+    const secret = request.headers['stripe-signature']
+
+    let event = Stripe.EventsResource
+
+    try { 
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (err) {
+
+    }
+
+    response.status(200).json({
+      ok: true
+    }) 
+  }
   console.log('Chegou')
 
-  response.status(200).json({
-    ok: true
-  })
 }
